4cb3a08f2e1349156e4fbb2fc5587344d937ded9
Luca Riotto <lucariotto709@gmail.com>
2025-01-11 17:56:58 +0100

    add new features

--- transaction_analysis_webapp.py
+++ transaction_analysis_webapp.py
@@ -3,22 +3,154 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from transaction_exploration import graphical_analysis
+import plotly.graph_objects as go
+import plotly.express as px
 
 path = '/Users/lucariotto/Documents/Personal/Gestione denaro/Analisi spese/Gestione entrate-spese.xlsx'
 transaction_df = pd.read_excel(path, sheet_name='Transazioni')
 
-# page title
-st.title("Transaction Analysis")
+# cleaning of data
+cols_to_keep = [
+    "DATA",
+    "GIORNO",
+    "MESE",
+    "ANNO",
+    "TIPO TRANSAZIONE",
+    "CONTO",
+    "CATEGORIA",
+    "SOTTOCATEGORIA",
+    "IMPORTO"
+]
+category_df = transaction_df.loc[transaction_df['CATEGORIE'].notna(), 'CATEGORIE']
+transaction_df = transaction_df[cols_to_keep]
+transaction_df.loc[:,'mese_anno'] = transaction_df['DATA'].dt.to_period('M')
+mese_anno_df = pd.DataFrame(transaction_df['mese_anno'].unique(), columns=['mese_anno'])
+df_outer_join = pd.merge(
+    mese_anno_df,  # seleziona solo la colonna 'mese_anno'
+    category_df,  # seleziona solo la colonna 'categoria'
+    how='cross',  # tipo di join (outer per includere tutte le combinazioni)
+    on=None,  # nessuna colonna su cui fare l'unione
+)
+df_outer_join.rename(columns={'CATEGORIE': 'CATEGORIA'}, inplace=True)
 
-# plot the monthly expenses 
-transact_plot = graphical_analysis(transaction_df)
-all_montly_exp = transact_plot.montly_ewma_plot(column_name='IMPORTO', alpha=0.1, date_name = 'DATA')
-st.pyplot(all_montly_exp)
+transaction_df = pd.merge(
+    df_outer_join,
+    transaction_df,
+    how='left',
+    on=['mese_anno', 'CATEGORIA']
+)
 
-# Monthly expenditure chart by category
+transaction_df['IMPORTO'] = np.where(transaction_df['IMPORTO'].isna(), 0, transaction_df['IMPORTO'])
+
+# fill some na column
+exit_category = [
+    "Casa",
+    "Spesa",
+    "Viaggi/Esperienze",
+    "Trasporti",
+    "Serate",
+    "Pasti fuori",
+    "Sport",
+    "Abbonamenti",
+    "Shopping",
+    "Altro"
+]
+transaction_df['TIPO TRANSAZIONE'] = np.where(transaction_df['CATEGORIA'].isin(exit_category), 'Uscita', 'Entrata')
+
+# inizialize data in graphical_analysis class 
 transact_plot = graphical_analysis(transaction_df)
-category = transaction_df['CATEGORIA'].unique().tolist()
-choice = st.selectbox("Select the category for compute the monthly expenses", category)
-st.write(f"Monthly expenditure chart for the {choice} category")
-choice_montly_exp=transact_plot.montly_ewma_plot(column_name='IMPORTO', alpha=0.1, date_name = 'DATA', category=choice)
-st.pyplot(choice_montly_exp)
+
+# page configuration
+st.set_page_config(
+    page_title="Transaction Analysis",  # Title of broswer page
+    page_icon=":bar_chart :",  
+    layout="wide"  # Layout 
+)
+page = st.sidebar.radio('Pages', options=['Expenses', 'Income', 'Assets'])
+
+# window of mooving average selection
+window = st.sidebar.number_input(
+        "Insert the window size for mooving average for all expenses plot",
+        min_value=2,  # min value
+        value=10,      # default value
+        step=1        # step
+        )
+
+if page == 'Expenses':
+    # title of the page
+    st.title("Analysis of expenses") 
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('<p style="font-size:24px; color:white;">Monthly expenses time series</p>', unsafe_allow_html=True)
+        st.write('Chart showing the aggregated monthly expenses and the moving average')
+        # plot the monthly expenses 
+        all_montly_exp = transact_plot.montly_ma_plot(column_name='IMPORTO', window=window, date_name = 'DATA')
+        st.plotly_chart(all_montly_exp)
+
+    with col2:
+        st.write('<p style="font-size:24px; color:whitw;">Monthly cashflow time series</p>', unsafe_allow_html=True)
+        st.write('Chart showing the aggregated monthly cashflof and the moving average')
+        # plot the monthly expenses 
+        # plot the monthly expenses 
+        cash_flow_plot = transact_plot.cash_flow_plot(column_name='IMPORTO', window=window, date_name = 'DATA')
+        st.plotly_chart(cash_flow_plot)
+
+    # expeses for category analysis
+    col1, col2 = st.columns(2)
+    with col1:
+        # table for montly expenses divided by category
+        category_expenses = (transaction_df[transaction_df['TIPO TRANSAZIONE'] == 'Uscita']
+                        .groupby(['mese_anno', 'CATEGORIA'])['IMPORTO']
+                        .sum().reset_index())
+        category_expenses = (category_expenses.groupby('CATEGORIA')['IMPORTO']
+                             .rolling(window=window, min_periods=1).mean()
+                             .reset_index().groupby('CATEGORIA')
+                            .last().drop(['level_1'], axis=1))
+        st.dataframe(category_expenses)  
+    
+    with col2:
+        category_expenses.reset_index(inplace=True)
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    labels=category_expenses['CATEGORIA'],        # Labels (categories)
+                    values=category_expenses['IMPORTO'],           # Values
+                    hoverinfo='label+percent+value', # Show label, percentage, and value on hover
+                    textinfo='percent',           # Display only percentage on the pie chart
+                    #marker=dict(colors=['#636EFA', '#EF553B', '#00CC96', '#AB63FA']),  # Custom colors
+                    )
+                ]
+        )
+        # Update layout to include a legend
+        fig.update_layout(
+            title="Category Distribution",
+            legend=dict(title="Categories", orientation="v", x=1, y=1),
+        )
+        st.plotly_chart(fig)
+
+
+    # expeses for category analysis graphical visualization
+    col1, col2 = st.columns(2)
+    with col1:
+        category = transaction_df.loc[transaction_df['TIPO TRANSAZIONE'] == 'Uscita', 'CATEGORIA'].unique().tolist()
+        choice1 = st.selectbox("Select the category for compute the monthly expenses", category,  key="category_selectbox")
+        if choice1 in exit_category:
+            type_transaction = 'Uscita'
+        else : 
+            type_transaction = 'Entrata'
+        st.write(f'<p style="font-size:24px; color:white;">Monthly expenditure chart for the {choice1} category</p>', 
+                unsafe_allow_html=True)
+        # plot the monthly expenses for category
+        choice1_montly_exp=transact_plot.montly_ma_plot(column_name='IMPORTO', window=10, date_name = 'DATA', 
+                                                        category=choice1, type_transaction=type_transaction)
+        st.plotly_chart(choice1_montly_exp)
+
+    with col2:
+        subcategory = transaction_df.loc[transaction_df['TIPO TRANSAZIONE'] == 'Uscita', 'SOTTOCATEGORIA'].unique().tolist()
+        subcategory = list(filter(pd.notna, subcategory))
+        choice2 = st.selectbox("Select the subcategory for compute the monthly expenses", subcategory,  key="subcategory_selectbox")
+        st.write(f'<p style="font-size:24px; color:white;">Monthly expenditure chart for the {choice2} subcategory</p>', 
+                unsafe_allow_html=True)
+        # plot the monthly expenses for category
+        choice2_montly_exp=transact_plot.montly_ma_plot(column_name='IMPORTO', window=10, date_name = 'DATA', sub_category=choice2)
+        st.plotly_chart(choice2_montly_exp)
--- transaction_exploration.py
+++ transaction_exploration.py
@@ -2,20 +2,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 # creation of a class for all grapichal analysis of transaction
 class graphical_analysis:
     def __init__(self, df):
         self.df = df
 
-    def montly_ewma_plot(self, column_name, alpha = 0.1, date_name = 'DATE', category = 'all', category_column = 'CATEGORIA',
+    def montly_ma_plot(self, column_name, window = 10, date_name = 'mese_anno', category = 'all', category_column = 'CATEGORIA',
                          type_transaction_col = 'TIPO TRANSAZIONE', type_transaction = 'Uscita', sub_category = 'all', 
                          sub_category_column = 'SOTTOCATEGORIA'):
         # function that perform an aggregation over month-year of the column and
         # plot the raw data and the ewma mean of the data over time
         # column_name : the name of the column to plot
         # date_name : the name of the column that contain the date, if missing date_name = 'DATE'
-        # alpha : the value of the parameter of the ewma statistic
+        # window : the value of the parameter of the mooving average
         # category : set the specific category to filter data
         # category_column : define the name of the column thath contain the different category of expenses
         # sub_category : set the specific sub category to filter data
@@ -23,73 +25,118 @@ class graphical_analysis:
         # type_transaction_col : col that define the type of transaction 
         # type_transaction : the type of transaction income or expenses
 
-        montly_ewma_df = self.df
-        if type_transaction_col not in montly_ewma_df.columns:
-            raise ValueError(f"type_transaction_col must be one of {[col for col in montly_ewma_df.columns if col != date_name]}")
-        if type_transaction not in montly_ewma_df[type_transaction_col].unique().tolist():
-            raise ValueError(f"Type transaction must be one of {montly_ewma_df[type_transaction_col].unique().tolist()}")
+        montly_ma_df = self.df
+        if type_transaction_col not in montly_ma_df.columns:
+            raise ValueError(f"type_transaction_col must be one of {[col for col in montly_ma_df.columns if col != date_name]}")
+        if type_transaction not in montly_ma_df[type_transaction_col].unique().tolist():
+            raise ValueError(f"Type transaction must be one of {montly_ma_df[type_transaction_col].unique().tolist()}")
             
-        montly_ewma_df = montly_ewma_df[montly_ewma_df[type_transaction_col] == type_transaction]
+        montly_ma_df = montly_ma_df[montly_ma_df[type_transaction_col] == type_transaction]
 
         if category != 'all':
-            if category_column not in montly_ewma_df.columns:
-                raise ValueError(f"category_column must be one of {[col for col in montly_ewma_df.columns if col != date_name]}")
-            if category not in montly_ewma_df[category_column].unique().tolist():
-                raise ValueError(f"category must be one of {montly_ewma_df[category_column].unique().tolist()}")
-            montly_ewma_df = montly_ewma_df[montly_ewma_df[category_column] == category]
+            if category_column not in montly_ma_df.columns:
+                raise ValueError(f"category_column must be one of {[col for col in montly_ma_df.columns if col != date_name]}")
+            if category not in montly_ma_df[category_column].unique().tolist():
+                raise ValueError(f"category must be one of {montly_ma_df[category_column].unique().tolist()}")
+            montly_ma_df = montly_ma_df[montly_ma_df[category_column] == category]
         
         if sub_category != 'all':
-            if sub_category_column not in montly_ewma_df.columns:
-                raise ValueError(f"sub category_column must be one of {[col for col in montly_ewma_df.columns if col != date_name]}")
-            if sub_category not in montly_ewma_df[sub_category_column].unique().tolist():
-                raise ValueError(f"sub category must be one of {montly_ewma_df[sub_category_column].unique().tolist()}")
-            montly_ewma_df = montly_ewma_df[montly_ewma_df[sub_category_column] == sub_category]
+            if sub_category_column not in montly_ma_df.columns:
+                raise ValueError(f"sub category_column must be one of {[col for col in montly_ma_df.columns if col != date_name]}")
+            if sub_category not in montly_ma_df[sub_category_column].unique().tolist():
+                raise ValueError(f"sub category must be one of {montly_ma_df[sub_category_column].unique().tolist()}")
+            montly_ma_df = montly_ma_df[montly_ma_df[sub_category_column] == sub_category]
 
-        montly_ewma_df.loc[:,'mese_anno'] = montly_ewma_df[date_name].dt.to_period('M')
-        montly_ewma_df = montly_ewma_df.groupby('mese_anno')[[column_name]].sum()
-        montly_ewma_df.reset_index(inplace=True)
-        montly_ewma_df['mese_anno'] = montly_ewma_df['mese_anno'].astype('str')
-        montly_ewma_df[f'{column_name}_ewma_05'] = montly_ewma_df[column_name].ewm(alpha=0.1, adjust=False).mean()
+        montly_ma_df = montly_ma_df.groupby('mese_anno')[[column_name]].sum()
+        montly_ma_df.reset_index(inplace=True)
+        montly_ma_df['mese_anno'] = montly_ma_df['mese_anno'].astype('str')
+        montly_ma_df[f'{column_name}_ma'] = montly_ma_df[column_name].rolling(window=window, min_periods=1).mean()
 
-        # plot the montly aggregation
-        plt.figure(figsize=(10, 6))  # Impostare le dimensioni del grafico
-        plt.plot(montly_ewma_df["mese_anno"], smontly_ewma_df[f"{column_name}_ewma_05"], 
-                marker="o", linestyle="-", color="b", label=f'ewma of montly {column_name}')
-        plt.plot(montly_ewma_df["mese_anno"], montly_ewma_df[column_name], 
-                marker="o", linestyle="-", color="r", label=[f'raw montly {column_name}'])
-        plt.xlabel("")
-        plt.ylabel(f"monthly {column_name}")
-        plt.title(f"Time series of monthly {column_name}")
-        plt.legend(loc='best')
-        
-        return plt
+        # create plot object
+        fig = go.Figure()
 
-    def cash_flow_plot(self, column_name, date_name = 'DATE', alpha=0.1, type_transaction_col = 'TIPO TRANSAZIONE'):
+        # add moving average line
+        fig.add_trace(go.Scatter(
+            x=montly_ma_df["mese_anno"], 
+            y=montly_ma_df[f"{column_name}_ma"], 
+            mode='lines+markers',
+            name='Moving average',
+            line=dict(color='blue'),
+            marker=dict(size=8)
+        ))
+
+        # add line for raw data
+        fig.add_trace(go.Scatter(
+            x=montly_ma_df["mese_anno"], 
+            y=montly_ma_df[column_name], 
+            mode='lines+markers',
+            name='Raw data',
+            line=dict(color='red'),
+            marker=dict(size=8)
+        ))
+
+        # custom layout
+        fig.update_layout(
+            title="Monthly Aggregation with Moving Average",
+            xaxis_title="Year-Month",
+            yaxis_title="Monthly Value",
+            legend=dict(title="Legend", x=0.8, y=1),
+            template="plotly_white",
+            hovermode="x unified" 
+        )
+
+        return fig
+
+    def cash_flow_plot(self, column_name, date_name = 'mese_anno', window=10, type_transaction_col = 'TIPO TRANSAZIONE'):
         # function that perform an aggregation over month-year of the column and
         # plot the the difference between income and expence
         # column_name : the name of the column to plot
         # date_name : the name of the column that contain the date, if missing date_name = 'DATE'
-         # alpha : the value of the parameter of the ewma statistic
+        # window : the value of the parameter of the mooving average
         # type_transaction_col : col that define the type of transaction 
+
+        cash_flow_df = self.df
+
         if type_transaction_col not in cash_flow_df.columns:
             raise ValueError(f"type_transaction_col must be one of {[col for col in cash_flow_df.columns if col != date_name]}")
         
-        cash_flow_df.loc[:,'mese_anno'] = cash_flow_df[date_name].dt.to_period('M')
         pivot_df = cash_flow_df.pivot_table(values=column_name, index='mese_anno', columns=type_transaction_col, aggfunc='sum')
         pivot_df.reset_index(inplace=True)
         pivot_df['cash_flow'] = pivot_df['Entrata'] - pivot_df['Uscita']
-        pivot_df['ewma_cash_flow'] = pivot_df['cash_flow'].ewm(alpha=0.1, adjust=False).mean()
+        pivot_df['ma_cash_flow'] = pivot_df['cash_flow'].rolling(window=window, min_periods=1).mean()
         pivot_df['mese_anno'] = pivot_df['mese_anno'].astype('str')
-        # plot the montly aggregation
-        plt.figure(figsize=(10, 6))  # Impostare le dimensioni del grafico
-        plt.plot(pivot_df["mese_anno"], pivot_df["ewma_cash_flow"], 
-                marker="o", linestyle="-", color="b", label='ewma_cash_flow')
-        plt.plot(pivot_df["mese_anno"], pivot_df['cash_flow'], 
-                marker="o", linestyle="-", color="r", label='cash_flow')
-        plt.xlabel("")
-        plt.ylabel("monthly cash_flow")
-        plt.title("Time series of monthly cash_flow")
-        plt.legend(loc='best')
         
-        return plt
+        # create plot object
+        fig = go.Figure()
+        # add line for mooving average of cash_flow
+        fig.add_trace(go.Scatter(
+            x=pivot_df["mese_anno"], 
+            y=pivot_df["ma_cash_flow"], 
+            mode='lines+markers',
+            name='Moving average cash flow',
+            line=dict(color='blue'),
+            marker=dict(size=8)
+        ))
+
+        # add line for raw cashflow
+        fig.add_trace(go.Scatter(
+            x=pivot_df["mese_anno"], 
+            y=pivot_df['cash_flow'], 
+            mode='lines+markers',
+            name='Raw cash flow',
+            line=dict(color='red'),
+            marker=dict(size=8)
+        ))
+
+        # custom layout
+        fig.update_layout(
+            title="Time Series of Monthly Cash Flow",
+            xaxis_title="Year-Month",
+            yaxis_title="Monthly Cash Flow",
+            legend=dict(title="Legend", x=0.8, y=1),
+            template="plotly_white",
+            hovermode="x unified" 
+        )
+            
+        return fig
         
